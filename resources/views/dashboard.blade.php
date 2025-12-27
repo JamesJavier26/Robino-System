740ee08be23ad49bde66b0795b1b9fdea36594ae
JamesJavier26 <131934803+JamesJavier26@users.noreply.github.com>
2025-12-27 20:59:17 +0800

    updated dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,12 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <div class="p-6 flex justify-center items-center">
+                    <img 
+                        src="{{ asset('assets/crosscourt.jpeg') }}" 
+                        alt="CrossCourt" 
+                        class="w-full max-w-5xl h-auto object-contain"
+                    />
                 </div>
             </div>
         </div>
