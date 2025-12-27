620e9578678022d4428de516756d83ba207d0c31
JamesJavier26 <131934803+JamesJavier26@users.noreply.github.com>
2025-12-27 21:03:42 +0800

    fixed user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -9,7 +9,12 @@
 
             <input name="password" type="password" placeholder="New Password (optional)" class="w-full mb-3">
             <input name="password_confirmation" type="password" placeholder="Confirm Password" class="w-full mb-3">
-
+            <select name="role" class="w-full mb-6 p-2 border rounded" required>
+                <option value="" disabled selected>Select Role</option>
+                <option value="admin">Admin</option>
+                <option value="user">User</option>
+            </select>
+                
             <button class="bg-blue-500 text-white px-4 py-2 rounded">Update</button>
         </form>
     </div>
